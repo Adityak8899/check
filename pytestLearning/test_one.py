612f3245649d3814974e69b94c848e942afb4993
Your Name <adityamznkanaujia@gmail.com>
2023-05-28 17:41:51 +0530

    change2

--- pytestLearning/test_one.py
+++ pytestLearning/test_one.py
@@ -3,3 +3,9 @@ import pytest
 def test_function():
     print("bhjafhakhfai")
     print("sgdfj")
+def test_function2():
+    print("bhjafhakhfai")
+    print("sgdfj")
+def test_function3():
+    print("bhjafhakhfai")
+    print("sgdfj")
